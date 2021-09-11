a036a19ea2725b314939acd8c5a72be55cb2b354
fushisanlang <313346216@qq.com>
2021-09-11 22:17:04 +0800

    修改mac系统clean后panic的bug

--- app/tools/clean.go
+++ app/tools/clean.go
@@ -25,7 +25,14 @@ func init() {
 		cmd.Run()
 
 	}
+	clear["darwin"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
 
+		cmd.Stdout = os.Stdout
+
+		cmd.Run()
+
+	}
 	clear["windows"] = func() {
 		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
 
